6ddb5b34beb536722b6c5f1bbd3300665f98f521
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-06-15 08:14:55 +0200

    Add CMSDK GPIO HAL

--- tools/HALs/HAL-CMSDK/Inc/cmsdk_gpio.h
+++ tools/HALs/HAL-CMSDK/Inc/cmsdk_gpio.h
@@ -16,12 +16,34 @@
 
 /***************************** Macros Definitions ****************************/
 
+#define GPIO_INPUT_MASK             (0u << 0)
+#define GPIO_OUTPUT_MASK            (1u << 0)
+#define GPIO_INTERRUPT_MASK         (1u << 1)
+#define GPIO_EDGE_MASK              (1u << 2)
+#define GPIO_LOW_OR_FALLING_MASK    (0u << 3)
+#define GPIO_HIGH_OR_RISING_MASK    (1u << 3)
+#define GPIO_ALTERNATE_MASK         (1u << 4)
+
+
+#define GPIO_MODE_ALTERNATIVE_FUNCTION      GPIO_ALTERNATE_MASK                                                                 /*!< Alternative function mode */
+#define GPIO_MODE_OUTPUT                    GPIO_OUTPUT_MASK                                                                    /*!< Output mode */
+#define GPIO_MODE_INPUT                     GPIO_INPUT_MASK                                                                     /*!< Input mode */
+#define GPIO_MODE_INTERRUPT_LOW             GPIO_INPUT_MASK | GPIO_INTERRUPT_MASK | GPIO_LOW_OR_FALLING_MASK                    /*!< Interrupt on pin reset mode */
+#define GPIO_MODE_INTERRUPT_HIGH            GPIO_INPUT_MASK | GPIO_INTERRUPT_MASK | GPIO_HIGH_OR_RISING_MASK                    /*!< Interrupt on pin set mode */
+#define GPIO_MODE_INTERRUPT_RISING_EDGE     GPIO_INPUT_MASK | GPIO_INTERRUPT_MASK | GPIO_EDGE_MASK | GPIO_LOW_OR_FALLING_MASK   /*!< Interrupt on pin rising edge mode */
+#define GPIO_MODE_INTERRUPT_FALLING_EDGE    GPIO_INPUT_MASK | GPIO_INTERRUPT_MASK | GPIO_EDGE_MASK | GPIO_HIGH_OR_RISING_MASK   /*!< Interrupt on pin falling edge mode */
+
 /***************************** Types Definitions *****************************/
 
 /**
  * @brief  GPIO port type redefinition
  */
 typedef CMSDK_GPIO_TypeDef GPIO_TypeDef;
+
+/**
+ * @brief  GPIO pin mode definition
+ */
+typedef uint32_t GPIO_ModeTypeDef;
 
 /**
  * @brief  GPIO Bit SET and Bit RESET enumeration
@@ -32,13 +54,14 @@ typedef enum
     GPIO_PIN_SET
 } GPIO_PinState;
 
+
 /*************************** Variables Declarations **************************/
 
 /*************************** Functions Declarations **************************/
 
-HAL_StatusTypeDef cmsdk_GpioInit(void);
-void cmsdk_GpioWritePin(GPIO_TypeDef *gpio, uint16_t pin, GPIO_PinState PinState);
-void cmsdk_GpioTogglePin(GPIO_TypeDef *gpio, uint16_t pin);
-GPIO_PinState cmsdk_GpioReadPin(GPIO_TypeDef *gpio, uint16_t pin);
+HAL_StatusTypeDef cmsdk_GpioInit(GPIO_TypeDef *gpio, uint16_t pin, GPIO_ModeTypeDef gpio_mode);
+HAL_StatusTypeDef cmsdk_GpioWritePin(GPIO_TypeDef *gpio, uint16_t pin, GPIO_PinState pin_state);
+HAL_StatusTypeDef cmsdk_GpioReadPin(GPIO_TypeDef *gpio, uint16_t pin, GPIO_PinState *pin_state);
+HAL_StatusTypeDef cmsdk_GpioTogglePin(GPIO_TypeDef *gpio, uint16_t pin);
 
 #endif /* CMSDK_GPIO_H */
--- tools/HALs/HAL-CMSDK/Src/cmsdk_gpio.c
+++ tools/HALs/HAL-CMSDK/Src/cmsdk_gpio.c
@@ -13,6 +13,21 @@
 
 /***************************** Macros Definitions ****************************/
 
+#define GPIO_PIN_MASK               (0x0000FFFFU) /* PIN mask for assert test */
+
+#define IS_GPIO_PIN(__PIN__)        ((((uint32_t)(__PIN__) & GPIO_PIN_MASK) != 0x00U) &&\
+                                     (((uint32_t)(__PIN__) & ~GPIO_PIN_MASK) == 0x00U))
+
+#define IS_GPIO_PIN_ACTION(ACTION)  (((ACTION) == GPIO_PIN_RESET) || ((ACTION) == GPIO_PIN_SET))
+
+#define IS_GPIO_MODE(MODE)          (((MODE) == GPIO_MODE_ALTERNATIVE_FUNCTION)     ||\
+                                     ((MODE) == GPIO_MODE_OUTPUT)                   ||\
+                                     ((MODE) == GPIO_MODE_INPUT)                    ||\
+                                     ((MODE) == GPIO_MODE_INTERRUPT_LOW)            ||\
+                                     ((MODE) == GPIO_MODE_INTERRUPT_HIGH)           ||\
+                                     ((MODE) == GPIO_MODE_INTERRUPT_RISING_EDGE)    ||\
+                                     ((MODE) == GPIO_MODE_INTERRUPT_FALLING_EDGE))
+
 /*************************** Functions Declarations **************************/
 
 /*************************** Variables Definitions ***************************/
@@ -22,31 +37,136 @@
 /**
  * @brief Init Gpio
  */
-HAL_StatusTypeDef cmsdk_GpioInit(void)
+HAL_StatusTypeDef cmsdk_GpioInit(GPIO_TypeDef  *gpio, uint16_t pin, GPIO_ModeTypeDef gpio_mode)
 {
+    HAL_StatusTypeDef status = HAL_OK;
 
+    if (IS_GPIO_PIN(pin) && IS_GPIO_MODE(gpio_mode))
+    {
+        if (gpio_mode & GPIO_ALTERNATE_MASK)
+        {
+            // Enable alternative function mode
+            gpio->ALTFUNCSET |= pin;
+        }
+        else
+        {
+            // Disable alternative function mode
+            gpio->ALTFUNCCLR |= pin;
+
+            // Check direction
+            if (gpio_mode & GPIO_OUTPUT_MASK)
+            {
+                // Enable output
+                gpio->OUTENABLESET |= pin;
+            }
+            else
+            {
+                // Disable output
+                gpio->OUTENABLECLR |= pin;
+
+                // Check Interrupt
+                if (gpio_mode & GPIO_INTERRUPT_MASK)
+                {
+                    // Check if we want the interrupt on edge or not
+                    if (gpio_mode & GPIO_EDGE_MASK)
+                    {
+                        gpio->INTTYPESET |= pin;
+                    }
+                    else
+                    {
+                        gpio->INTTYPECLR |= pin;
+                    }
+
+                    // Check the polarity of the interrupt
+                    if (gpio_mode & GPIO_HIGH_OR_RISING_MASK)
+                    {
+                        // Set HIGH level or rising edge
+                        gpio->INTPOLSET |= pin;
+                    }
+                    else
+                    {
+                        // Set LOW level or falling edge
+                        gpio->INTPOLCLR |= pin;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        status = HAL_ERROR;
+    }
+
+    return status;
 }
 
 /**
  * @brief Write a specific pin from a gpio
  */
-void cmsdk_GpioWritePin(GPIO_TypeDef *gpio, uint16_t pin, GPIO_PinState PinState)
+HAL_StatusTypeDef cmsdk_GpioWritePin(GPIO_TypeDef *gpio, uint16_t pin, GPIO_PinState pin_state)
 {
+    HAL_StatusTypeDef status = HAL_OK;
 
-}
+    if (IS_GPIO_PIN(pin) & IS_GPIO_PIN_ACTION(pin_state))
+    {
+        if (pin_state == GPIO_PIN_SET)
+        {
+            gpio->DATAOUT |= pin;
+        }
+        else
+        {
+            gpio->DATAOUT &= ~pin;
+        }
+    }
+    else
+    {
+        status = HAL_ERROR;
+    }
 
-/**
- * @brief Toggles a specific pin from a gpio
- */
-void cmsdk_GpioTogglePin(GPIO_TypeDef *gpio, uint16_t pin)
-{
-
+    return status;
 }
 
 /**
  * @brief Read a specific pin from a gpio
  */
-GPIO_PinState cmsdk_GpioReadPin(GPIO_TypeDef *gpio, uint16_t pin)
+HAL_StatusTypeDef cmsdk_GpioReadPin(GPIO_TypeDef *gpio, uint16_t pin, GPIO_PinState *pin_state)
 {
+    HAL_StatusTypeDef status = HAL_OK;
 
+    if (IS_GPIO_PIN(pin))
+    {
+        if ((gpio->DATA & pin) != 0x0000u)
+        {
+            *pin_state = GPIO_PIN_SET;
+        }
+        else
+        {
+            *pin_state = GPIO_PIN_RESET;
+        }
+    }
+    else
+    {
+        status = HAL_ERROR;
+    }
+
+    return status;
+}
+
+/**
+ * @brief Toggles a specific pin from a gpio
+ */
+HAL_StatusTypeDef cmsdk_GpioTogglePin(GPIO_TypeDef *gpio, uint16_t pin)
+{
+    HAL_StatusTypeDef status = HAL_OK;
+
+    if (IS_GPIO_PIN(pin))
+    {
+        gpio->DATAOUT ^= pin;
+    }
+    else
+    {
+        status = HAL_ERROR;
+    }
+
+    return status;
 }
